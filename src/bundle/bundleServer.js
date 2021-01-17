8356ca7d859c70c26c17eb8878fa06e977ea88d1
Your Name <jazz@tezle.com>
2021-01-17 23:29:29 +0000

    Changed server now bundled with webpack rollup removed

--- src/bundle/bundleServer.js
+++ src/bundle/bundleServer.js
@@ -1,52 +1,35 @@
 import path from 'path';
 import fs from 'fs';
-
-import rollup from 'rollup';
-import ruResolve from 'rollup-plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+import webpack from 'webpack';
 
 import { bundleServer as lg } from './logger';
 import timer from '../util/timer';
 
-const terserOptions = {
-  mangle: {
-    reserved: ['doGet', 'apiListener'],
-    toplevel: false
-  },
-  compress: {
-    toplevel: false,
-    unused: false
-  }
-};
+// eslint-disable-next-line consistent-return
+const build = (options) => new Promise((resolve, reject) => {
+  if (options.server.skip) return resolve();
+  const bannerText = fs.readFileSync(path.resolve(__dirname, '../src/api/serverAppend.js')).toString();
 
-async function build(options) {
-  if (options.server.skip) return Promise.resolve();
-  // see below for details on the options
-  const inputOptions = {
-    input: path.join(process.cwd(), '/server/index.js'),
-    plugins: [ruResolve(), options.server.compress && terser(terserOptions)],
-    context: process.cwd()
-  };
-  const outputOptions = {
-    file: path.join(process.cwd(), '/clasp/api.js'),
-    compact: false,
-    format: 'es',
-    globals: ['api'],
-    banner: `${fs.readFileSync(path.resolve(__dirname, '../src/api/serverAppend.js'))}{`,
-    footer: '}'
-  };
+  const tm = timer().start();
+  lg.start();
 
-  return new Promise((resolve) => {
-    const tm = timer().start();
-    lg.start();
-    // create a bundle
-    rollup.rollup(inputOptions)
-      .then((bundle) => bundle.write(outputOptions))
-      .then(() => {
-        lg.success(tm.end());
-        resolve();
-      });
+  webpack({
+    entry: path.join(process.cwd(), '/server/api.js'),
+    plugins: [
+      new webpack.BannerPlugin({ banner: bannerText, raw: true })
+    ],
+    output: {
+      filename: 'path.js',
+      path: path.join(process.cwd(), 'clasp'),
+    },
+  }, (err, stats) => { // [Stats Object](#stats-object)
+    if (err || stats.hasErrors()) {
+      reject();
+    }
+
+    lg.success(tm.end());
+    resolve();
   });
-}
+});
 
 export default build;
